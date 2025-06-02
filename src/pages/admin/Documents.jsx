b938af52d2e0ac05e1def90829ff7607f5d2716f
Nadia-FTS <nadia@fabtechsol.com>
2025-06-02 12:28:21 +0500

    Implement code changes to enhance functionality and improve performance

--- src/pages/admin/Documents.jsx
+++ src/pages/admin/Documents.jsx
@@ -1,9 +1,115 @@
 import React from 'react'
+import CompliantOne from '../../assets/images/comliant1.png'
+import { GoDotFill } from 'react-icons/go';
 
 export default function Documents() {
+  const cards = [
+    { title: "Documents Expiring Soon", value: "9", },
+    { title: "Incomplete Profiles", value: "6", },
+    { title: "Pending Verifications", value: "4", },
+    { title: "Total Documents", value: "314" },
+  ];
+
+  const compliant = [
+    {
+      comImg: CompliantOne,
+      compliantCard: "Compliant",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Non-compliant",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Compliant",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Pending",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Non-compliant",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Compliant",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+    {
+      comImg: CompliantOne,
+      compliantCard: "Pending",
+      title: "Driver's License - John Doe",
+      state: "California",
+      expire: "Feb 1, 2025"
+    },
+  ]
+
+  const getColor = (status) => {
+    switch (status) {
+      case "Compliant":
+        return "bg-[#22C55E]";
+      case "Non-compliant":
+        return "bg-[#F43F5E]";
+      case "Pending":
+        return "bg-[#FACC15]";
+      default:
+        return "";
+    }
+  };
   return (
     <div>
-      Documents
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {cards.map((card, index) => (
+          <div key={index} className="border border-main p-4 rounded-[16px] bg-sidebar flex flex-col justify-between h-full">
+            <p className="c-secondary text-sm">{card.title}</p>
+            <p className="text-xl font-semibold c-primary mt-2">{card.value}</p>
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-5">
+        {compliant.map((doc, index) => (
+          <div 
+            key={index} 
+            className='border border-main p-4 rounded-[16px] flex flex-col justify-between h-full'
+          >
+            <div className='relative'>
+              <img src={doc.comImg} alt="" className='rounded-md brightness-75'/>
+              <div className={`absolute top-1 right-1 py-1 px-2 rounded-full text-xs ${getColor(doc.compliantCard)}`}>
+                  <p className='text-white flex gap-1 items-center'><GoDotFill />{doc.compliantCard}</p>
+              </div>
+            </div>
+            <div className='mt-3'>
+              <p className="c-secondary text-base">{doc.title}</p>
+              <div className='flex items-center gap-1'>
+                <p className="text-sm c-secondary mt-2">State:</p>
+                <p className="text-sm c-secondary mt-2">{doc.state}</p>
+              </div>
+              <div className='flex items-center gap-1'>
+                <p className="text-sm c-secondary mt-2">Expires:</p>
+                <p className="text-sm c-secondary mt-2">{doc.expire}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/pages/admin/PayrollOverview.jsx
+++ src/pages/admin/PayrollOverview.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export default function PayrollOverview() {
+  const cards = [
+    { title: "Total Payroll", value: "$124,000", },
+    { title: "Pending Payments", value: "$18,450", },
+    { title: "Employees Paid", value: "124 / 140", },
+    { title: "Next Payroll Date", value: "June 5, 2025" },
+  ];
   return (
     <div>
-      PayrollOverview
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {cards.map((card, index) => (
+          <div key={index} className="border border-main p-4 rounded-[16px] bg-sidebar flex flex-col justify-between h-full">
+            <p className="c-secondary text-sm">{card.title}</p>
+            <p className="text-xl font-semibold c-primary mt-2">{card.value}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
