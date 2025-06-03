f06607daf699ac2c5baa64e7dfb6f8aa09cf42d5
Nadia-FTS <nadia@fabtechsol.com>
2025-06-03 14:22:06 +0500

    Add status color coding for "Needs Check" in LiveMap component

--- src/pages/admin/LiveMap.jsx
+++ src/pages/admin/LiveMap.jsx
@@ -196,7 +196,9 @@ export default function LiveMap() {
                     ? "2"
                     : item.status.toLowerCase() === "charging"
                     ? "3"
-                    : "4";
+                    : item.status.toLowerCase() === "needs check"
+                    ? "4"
+                    : "5";
                   return (
                     <div
                       key={index}
@@ -237,6 +239,8 @@ export default function LiveMap() {
                               ? "bg-[var(--bgdanger)]"
                               : status === "3"
                               ? "bg-[var(--bgatblue)]"
+                              : status === "4"
+                              ? "bg-[var(--csecondary)]"
                               : "bg-[var(--bgwarning)]"
                           }`}
                         >
@@ -248,6 +252,8 @@ export default function LiveMap() {
                                 ? "bg-[var(--cdanger)]"
                                 : status === "3"
                                 ? "bg-[var(--catblue)]"
+                                : status === "4"
+                                ? "bg-[var(--cinverted]"
                                 : "bg-[var(--cwarning)]"
                             }`}
                           ></div>
@@ -259,6 +265,8 @@ export default function LiveMap() {
                                 ? "text-[var(--cdanger)]"
                                 : status === "3"
                                 ? "text-[var(--catblue)]"
+                                : status === "4"
+                                ? "text-[var(--cinverted)]"
                                 : "text-[var(--cwarning)]"
                             }`}
                           >
